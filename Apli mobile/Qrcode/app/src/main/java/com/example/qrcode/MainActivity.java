55c42649ac59c6c983570d6e74b2faae99e63978
Yanis <yaniboussaid.pro@gmail.com>
2020-03-06 18:31:44 +0100

    Ajout Appli mobile

---
+++ Apli mobile/Qrcode/app/src/main/java/com/example/qrcode/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.qrcode;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+
+
+import me.dm7.barcodescanner.zxing.ZXingScannerView;
+
+
+public class MainActivity extends AppCompatActivity {
+    public static TextView resultTextView;
+
+    Button scan_btn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        resultTextView = (TextView)findViewById(R.id.result_text);
+        scan_btn = (Button) findViewById(R.id.btn_scan);
+        scan_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                startActivity(new Intent(getApplicationContext(),ScanCodeActivity.class));
+            }
+        });
+    }
+}
